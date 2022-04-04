0bff05fd99c9b4a2827cabfbb3312fe2a97977ed
Your Name <louiestokk>
2022-04-04 22:43:22 +0200

    updated banner sections with lotti filea and styles

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,6 +4,10 @@ import { FaRegHospital } from "react-icons/fa";
 import { FaTruck } from "react-icons/fa";
 import { MdOutlineLocalHospital } from "react-icons/md";
 import { FaTruckLoading } from "react-icons/fa";
+import { VardLottie } from "../../lottie/VardLottie";
+import care from "../../utils/animation/care.json";
+import delivery from "../../utils/animation/delivery.json";
+import TransportLottie from "../../lottie/TransportLottie";
 const Banner = () => {
   return (
     <div className="banner">
@@ -11,8 +15,9 @@ const Banner = () => {
       <section>
         <div className="category">
           <article className="article">
-            <h4>Vård</h4>
-            <MdOutlineLocalHospital className="banner-icon" />
+            <div>
+              <VardLottie lotti={care} heigth={200} width={200} />
+            </div>
           </article>
           <div className="banner-card">
             <p className="banner-card-p">Lista annonser här</p>
@@ -20,26 +25,9 @@ const Banner = () => {
         </div>
         <div className="category">
           <article className="article">
-            <h4>Omsorg</h4>
-            <FaRegHospital className="banner-icon" />
-          </article>
-          <div className="banner-card">
-            <p className="banner-card-p">Lista annonser här</p>
-          </div>
-        </div>
-        <div className="category">
-          <article className="article">
-            <h4>Transport</h4>
-            <FaTruck className="banner-icon" />
-          </article>
-          <div className="banner-card">
-            <p className="banner-card-p">Lista annonser här</p>
-          </div>
-        </div>
-        <div className="category">
-          <article className="article">
-            <h4>Logistik</h4>
-            <FaTruckLoading className="banner-icon" />
+            <div>
+              <TransportLottie lotti={delivery} heigth={200} width={200} />
+            </div>
           </article>
           <div className="banner-card">
             <p className="banner-card-p">Lista annonser här</p>
