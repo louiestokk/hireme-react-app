7f56f57c1f017025bd0eff663bfa7b9e44e48a91
Your Name <louiestokk>
2022-04-07 15:45:33 +0200

    styles

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -12,7 +12,7 @@ const Services = () => {
       </p>
       <div className="service">
         <div className="service-item">
-          <h5>Arbetsgivare</h5>
+          <h5>Arbetsgivare rörlig</h5>
           <p>
             Tjänsten där ni som arbetsgivare tar kontakt och får ansökningar
             från kandidater. Givetvis betalar ni endast när ni vill få
@@ -28,6 +28,27 @@ const Services = () => {
               <p>
                 {<AiFillCheckCircle />}
                 <span>Betala endast 29kr för kontaktuppgifter</span>
+              </p>
+            </div>
+          </div>
+          <button>Kom igångs kostnadsfritt </button>
+        </div>
+        <div className="service-item">
+          <h5>Arbetsgivare fast</h5>
+          <p>
+            Tjänsten där ni som arbetsgivare tar kontakt och får ansökningar
+            från kandidater. Obegränsat med kandidater och kontakter.
+          </p>
+          <div className="rekrytering">
+            <h5>Rekrytering</h5>
+
+            <div className="rek-item">
+              <p>
+                {<AiFillCheckCircle />} <span>Okr per lyckad rekrytering</span>
+              </p>
+              <p>
+                {<AiFillCheckCircle />}
+                <span>Betala 2999kr/mån "inga bindningstider"</span>
               </p>
             </div>
           </div>
